95eac2c822cd2081171dc22d09aa3b3500b4c699
candyisthebest2004 <mrunmayee.wankhede@gmail.com>
2026-03-22 17:22:18 -0400

    Added merge_all_49.py from my branch

---
+++ Scripts/merge_all_49.py
@@ -0,0 +1,35 @@
+import scanpy as sc
+import os
+
+top_folder = "/Users/mrunmayeewankhede/Desktop/cmu/Spring 26/Machine Learning for Scientists/Project/selected_49_samples"
+
+adatas = []
+
+for root, dirs, files in os.walk(top_folder):
+    for f in files:
+        if f.endswith(".h5"):
+            full_path = os.path.join(root, f)
+            print("Loading:", full_path)
+            adata = sc.read_10x_h5(full_path)
+            adata.var_names_make_unique()       # fix per-sample first
+            sample_id = os.path.basename(root)
+            adata.obs['sample_id'] = sample_id
+            adatas.append(adata)
+
+print(f"Total files loaded: {len(adatas)}")
+
+if len(adatas) == 0:
+    raise ValueError("No .h5 files found! Check folder path.")
+
+combined = sc.concat(
+    adatas,
+    join='outer',
+    label='sample_id',
+    keys=[a.obs['sample_id'][0] for a in adatas],
+    index_unique='-'   # avoids cell barcode collisions across samples
+)
+
+print("Combined shape:", combined.shape)
+
+combined.write("/Users/mrunmayeewankhede/Desktop/merged_49_samples.h5ad")
+print("Saved merged_49_samples.h5ad")
